24f6442cdaa3c990db4d1c5b088929916cff31cf
Vakle3360 <64584990+Vakle3360@users.noreply.github.com>
2023-03-10 22:51:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function access() {
 
 function checkForAccess() {
     if (document.cookie == "Expresso=38196u790uyf980y908auy9310uyr0fuy903u109u09u901u9un09fd7u390q7f097q90f8n903q8f09q") {
-        window.location.replace("http://vakle.tech/expresso.html")
+        window.location.replace("expresso.html")
     }
     else {
-        window.location.replace("http://vakle.tech/")
+        window.location.replace("")
     }
 }
